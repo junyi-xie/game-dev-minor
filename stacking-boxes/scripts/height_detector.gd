9b7bb43f899fe88e87bfe409368675510e6d7268
Junyi Xie <yunyi.xie@outlook.com>
2025-03-14 16:22:37 +0100

    gold 2

---
+++ stacking-boxes/scripts/height_detector.gd
@@ -0,0 +1,13 @@
+extends RayCast3D
+
+signal height_updated(new_height: float)
+
+var highest_point: float = 0.0
+
+func _physics_process(_delta: float) -> void:
+	if is_colliding():
+		
+		var collision_point = get_collision_point()
+		if collision_point.y > highest_point + 0.01:
+			highest_point = collision_point.y
+			emit_signal("height_updated", highest_point)
---
+++ stacking-boxes/utilities/autoload/signal_manager.gd
@@ -0,0 +1,5 @@
+extends Node
+
+signal game_over
+signal game_retry
+signal game_quit
